7270ee68c64a1a331156f29fd270c46a444a3d5a
lxbig <lxbcocos@163.com>
2017-10-25 14:58:43 +0800

    在分支1上输入内容

--- GitTest/GitTest/main.cpp
+++ GitTest/GitTest/main.cpp
@@ -13,5 +13,11 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     printf("Hello Beijing\n");
     //这是分支1的操作
+    /*
+     合并分支
+     同步到服务端
+     删除分支1
+     同步到服务端
+     */
     return 0;
 }
